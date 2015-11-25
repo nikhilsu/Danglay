fda69df54b34d00d3e520764157974b308e0ccbd
rijun <rijun@thoughtworks.com>
2015-11-25 15:54:06 +0530

    [Kusum|Riju] #1 - Spec for successfull user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,17 +7,21 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-
+      redirect_to @user
     else
       render 'new'
     end
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   private
 
   def user_params
-    allowed_params = params.require(:user).permit(:name, :emp_id, :email, :address)
-    locality = Locality.find_by(name: params[:locality])
+    allowed_params = params.require(:user).permit(:name, :emp_id, :email, :address, :locality)
+    locality = Locality.find(params[:user][:locality])
     allowed_params.merge(locality: locality)
   end
 end
--- spec/requests/user_signups_spec.rb
+++ spec/requests/user_signups_spec.rb
@@ -2,14 +2,25 @@ require 'rails_helper'
 
 RSpec.describe "UserSignups", type: :request do
   describe 'user signup' do
+
     it 'sign up with empty address should throw error' do
       get signup_path
       expect(response).to render_template(:new)
-      locality_name = build(:locality).name
+      locality_id = create(:locality).id
 
-      post '/users', :user => { address: '', locality: locality_name }
+      post '/users', :user => { name: 'User', emp_id: 12345, email: 'user@example', address: '', locality: locality_id }
       expect(response).to render_template(:new)
       expect(response.body).to include 'error'
+    end
+
+    it 'sign up with valid user' do
+      get signup_path
+      expect(response).to render_template(:new)
+      locality_id = create(:locality).id
+
+      post '/users', :user => { name: 'User', emp_id: 12345, email: 'user@example', address: 'Address', locality: locality_id }
+      follow_redirect!
+      expect(response).to render_template(:show)
     end
   end
 end
