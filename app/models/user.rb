c3229b1855fc68cc87c421dd8c76d72f1d436bad
rijun <rijun@thoughtworks.com>
2015-11-25 10:17:09 +0530

    [Kusum|Riju] #Misc - Refactored User Active Record

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,4 @@
 class User < ActiveRecord::Base
   belongs_to :locality
-  validates_presence_of :emp_id
-  validates_presence_of :name
-  validates_presence_of :email
-  validates_presence_of :address
-  validates_presence_of :locality_id
+  validates_presence_of :emp_id, :name, :email, :address, :locality
 end
