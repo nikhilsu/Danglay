9c8c7d52ff803e36be090622681c019407f52287
rijun <rijun@thoughtworks.com>
2015-11-27 09:57:52 +0530

    [SP|Riju] - Added specs to user controller to test invalid posts, removed request specs

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,9 +3,29 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
 
   describe 'GET new' do
+    before(:each) do
+      session[:userid] = 1
+    end
+
     it 'should render new page' do
       get :new
       expect(response).to render_template('new')
+    end
+
+    it 'should render new page on invalid signup' do
+      locality_id = create(:locality).id
+      post :create, :user => { name: 'User', emp_id: 12345, email: 'user@example', address: '', locality: locality_id }
+      user = assigns(:user)
+      expect(response).to render_template('new')
+      expect(user.errors.any?).to be true
+    end
+
+    it 'should render show page on valid signup' do
+      locality_id = create(:locality).id
+      post :create, :user => { name: 'User', emp_id: 12345, email: 'user@example', address: 'Address', locality: locality_id }
+      user = assigns(:user)
+      expect(response).to redirect_to(user)
+      expect(user.errors.any?).to be false
     end
   end
 end
