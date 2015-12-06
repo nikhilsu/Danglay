2c2124d10debe4d72e9c987b80d4e6aabb34c436
Abhishek S P <abhisheksp1993@gmail.com>
2015-12-06 17:45:01 +0530

    [SP] #2 [Refactor] Moved inline styling to cabpool stylesheet

--- app/assets/javascripts/cabpool.js
+++ app/assets/javascripts/cabpool.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
                 '<div class="col-md-5 col-md-offset-3"> ' +
                     '<div class="localityForm">' + $('div.localityForm').html() + '</div> ' +
                 '</div> ' +
-                '<a href="javascript:;"><span class="glyphicon glyphicon-minus-sign" id="removeNewLocality"style="color: #990000; line-height: 2"></span></a> ' +
+                '<a href="javascript:;"><span class="glyphicon glyphicon-minus-sign" id="removeNewLocality"></span></a> ' +
             '</div>');
 
             var select_tag = $('div.localityForm:last > select')
