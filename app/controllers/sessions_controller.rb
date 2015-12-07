ac5a83c878b79c775523be8e1e91c5428734745f
dsandeephegde <sandeeph@thoughtworks.com>
2015-12-07 16:25:13 +0530

    [Sandeep]  Added corresponding logout url for staging environment

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,12 @@ class SessionsController < ApplicationController
 
   def destroy
     delete_session
+    debugger
+    if Rails.env != 'staging'
     redirect_to 'https://dev-774694.oktapreview.com'
+    else
+      redirect_to 'https://dev-846101.oktapreview.com'
+    end
   end
 
   private
