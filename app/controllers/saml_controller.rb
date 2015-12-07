03e4b3add9533a0492ff0c92d5a34427d33d3cfc
rijun <rijun@thoughtworks.com>
2015-12-07 13:18:52 +0530

    Configure

--- app/controllers/saml_controller.rb
+++ app/controllers/saml_controller.rb
@@ -62,6 +62,23 @@ class SamlController < ApplicationController
       gyzbUG4DFys0jf5HAp6pVVAceKGwR+DDdXFU+vboHduCGoaUhAMDUQGmyD0JRgkqmikaF+si/3AB
       /6+omCKrcNI3Zzdex8fokRdP17H21QLj</ds:X509Certificate></ds:X509Data></ds:KeyInfo></md:KeyDescriptor><md:SingleLogoutService Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST" Location="https://dev-774694.oktapreview.com/app/thoughtworksdev774694_railsoktatest_1/exk5fn90zik3RdpS30h7/slo/saml"/><md:NameIDFormat>urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress</md:NameIDFormat><md:NameIDFormat>urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified</md:NameIDFormat><md:SingleSignOnService Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST" Location="https://dev-774694.oktapreview.com/app/thoughtworksdev774694_railsoktatest_1/exk5fn90zik3RdpS30h7/sso/saml"/><md:SingleSignOnService Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect" Location="https://dev-774694.oktapreview.com/app/thoughtworksdev774694_railsoktatest_1/exk5fn90zik3RdpS30h7/sso/saml"/></md:IDPSSODescriptor></md:EntityDescriptor>',
 
+      'qa' => '<?xml version="1.0" encoding="UTF-8"?><md:EntityDescriptor xmlns:md="urn:oasis:names:tc:SAML:2.0:metadata" entityID="http://www.okta.com/exk5fvjra2CUKZq1f0h7"><md:IDPSSODescriptor WantAuthnRequestsSigned="true" protocolSupportEnumeration="urn:oasis:names:tc:SAML:2.0:protocol"><md:KeyDescriptor use="signing"><ds:KeyInfo xmlns:ds="http://www.w3.org/2000/09/xmldsig#"><ds:X509Data><ds:X509Certificate>MIIDpDCCAoygAwIBAgIGAVE5/l75MA0GCSqGSIb3DQEBBQUAMIGSMQswCQYDVQQGEwJVUzETMBEG
+      A1UECAwKQ2FsaWZvcm5pYTEWMBQGA1UEBwwNU2FuIEZyYW5jaXNjbzENMAsGA1UECgwET2t0YTEU
+      MBIGA1UECwwLU1NPUHJvdmlkZXIxEzARBgNVBAMMCmRldi03NzQ2OTQxHDAaBgkqhkiG9w0BCQEW
+      DWluZm9Ab2t0YS5jb20wHhcNMTUxMTI0MTQ1NDUwWhcNMjUxMTI0MTQ1NTUwWjCBkjELMAkGA1UE
+      BhMCVVMxEzARBgNVBAgMCkNhbGlmb3JuaWExFjAUBgNVBAcMDVNhbiBGcmFuY2lzY28xDTALBgNV
+      BAoMBE9rdGExFDASBgNVBAsMC1NTT1Byb3ZpZGVyMRMwEQYDVQQDDApkZXYtNzc0Njk0MRwwGgYJ
+      KoZIhvcNAQkBFg1pbmZvQG9rdGEuY29tMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA
+      8qowgy2O0W0W6WVPEqTxeTCK/unYLu46WYIh/mNKyHEpzTCng1eu0IE1pViqhVi7uhJ43CSbQXFa
+      TA0cjeIf1I/6hUj+eYdNySLYhDKa+m/VW3kj4NMpCC4POTYOZVMkC9dM8V9ysjpC5WlIW8avuhqZ
+      XVlVbF3Ibj+isNqzUu443LWh75Z/wQ88l+zb5kh6cm1zDpw0PZeC89n2hmqwFJUoMwPtHKPyzAFB
+      dbm2R5hFcV6NLkThE5Q5F/HIt7qlZw7l/IRSQHykeoIvVGAfRsrTnBVo+UUL4WGgYGVwoNb3qLFs
+      q49+BNyFtRhHoc/Jqzqug3PoY6vbjoCqvEsniQIDAQABMA0GCSqGSIb3DQEBBQUAA4IBAQDm+Xz3
+      HLiUwJH4tSEdzxafrH0uwwljptqyLiNIzFl2CirHErOeryvdegnu9cUtvadL7VWSBtHo0bmDAGwX
+      ki2oqTUrXDtp0N2vCJLQ19MCtPGDMMufbRhW3e/uoKELmJPPPuvAnQ9h50naWRK4d6E80ReYvrXa
+      NkxRGyv336gej4oe8wuCcawf0oN+YUZoUDg9Q57+j6Md4OWGOQ/d328b8AKlrkzJj26zyoAZFLkj
+      gyzbUG4DFys0jf5HAp6pVVAceKGwR+DDdXFU+vboHduCGoaUhAMDUQGmyD0JRgkqmikaF+si/3AB',
+
       'test' => '<?xml version="1.0" encoding="UTF-8"?><md:EntityDescriptor xmlns:md="urn:oasis:names:tc:SAML:2.0:metadata" entityID="http://www.okta.com/exk5fn90zik3RdpS30h7"><md:IDPSSODescriptor WantAuthnRequestsSigned="true" protocolSupportEnumeration="urn:oasis:names:tc:SAML:2.0:protocol"><md:KeyDescriptor use="signing"><ds:KeyInfo xmlns:ds="http://www.w3.org/2000/09/xmldsig#"><ds:X509Data><ds:X509Certificate>MIIDpDCCAoygAwIBAgIGAVE5/l75MA0GCSqGSIb3DQEBBQUAMIGSMQswCQYDVQQGEwJVUzETMBEG
       A1UECAwKQ2FsaWZvcm5pYTEWMBQGA1UEBwwNU2FuIEZyYW5jaXNjbzENMAsGA1UECgwET2t0YTEU
       MBIGA1UECwwLU1NPUHJvdmlkZXIxEzARBgNVBAMMCmRldi03NzQ2OTQxHDAaBgkqhkiG9w0BCQEW
