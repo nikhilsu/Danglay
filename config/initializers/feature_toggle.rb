75ab32eb216773cc2aae5d733517ae64b457c670
dsandeephegde <sandeeph@thoughtworks.com>
2015-12-08 14:35:24 +0530

    [SP | SU] #Misc activated all features in prod for testing

--- config/initializers/feature_toggle.rb
+++ config/initializers/feature_toggle.rb
@@ -17,5 +17,5 @@ if Rails.env.staging?
 end
 
 if Rails.env.production?
-  FEATURES.deactivate('user_feature', 'okta_feature', 'create_cabpool_feature')
+  FEATURES.activate('user_feature', 'okta_feature', 'create_cabpool_feature')
 end
