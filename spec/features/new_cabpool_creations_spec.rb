57ca472e749f373237e6ff996e2175ef310bb028
Deepika Vasudevan <deepikav2793@gmail.com>
2015-12-14 16:02:24 +0530

    [Deepika] Moved rendering of welcome page to create and show cab pool pages. Changed label names in new cabpool creation"

--- spec/features/new_cabpool_creations_spec.rb
+++ spec/features/new_cabpool_creations_spec.rb
@@ -11,18 +11,18 @@ RSpec.feature "NewCabpoolCreations", type: :feature do
 
   scenario 'valid creation of cabpool', js: true do
     visit new_cabpool_path
-    fill_in 'Number of people to travel with', with: ''
-    fill_in 'Pick-up time to office', with: ''
-    fill_in 'Pick-up time from office', with: ''
+    fill_in 'Capacity of Cab', with: ''
+    fill_in 'Arrival time to office', with: ''
+    fill_in 'Departure time from office', with: ''
     page.execute_script "window.scrollBy(0,10000)"
     click_button 'Create a pool'
     expect(page.current_path).to eq '/cabpools'
     expect(page.body).to have_content("This is not a number.")
     expect(page.body).to have_content("is not a valid time")
     visit new_cabpool_path
-    fill_in 'Number of people to travel with', with: '3'
-    fill_in 'Pick-up time to office', with: '12:23 PM'
-    fill_in 'Pick-up time from office', with: '9:00 PM'
+    fill_in 'Capacity of Cab', with: '3'
+    fill_in 'Arrival time to office', with: '12:23 PM'
+    fill_in 'Departure time from office', with: '9:00 PM'
     find('#addLocality').trigger('click')
     find(:xpath, '//*[@id="localitySelections"]/div[1]/div/div/div[1]/input').set("HAL")
     find(:xpath, '//*[@id="localitySelections"]/div[1]/div/div/div[1]/input').native.send_keys(:return)
