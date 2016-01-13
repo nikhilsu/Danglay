7864ed08060e18b9cba88258a48a305c39916ffd
sandeeph <sandeeph@thoughtworks.com>
2016-01-13 16:49:02 +0530

    [Sandeep|Kusumi] #38 UI Integration - Register user form.

--- spec/features/user_signups_spec.rb
+++ spec/features/user_signups_spec.rb
@@ -10,28 +10,30 @@ RSpec.describe "UserSignups", type: :feature do
 
   scenario 'valid user signup with existing location' do
     visit new_user_path
-    fill_in 'Employee ID', with: '12345'
-    fill_in 'Address', with: 'No.322, Mars'
-    fill_in 'Phone No', with: '+91 9080706055'
-    select('Banaswadi', from: 'Locality')
+    fill_in 'user_emp_id', with: '12345'
+    fill_in 'user_address', with: 'No.322, Mars'
+    fill_in 'user_phone_no', with: '+91 9080706055'
+    select('Banaswadi', from: 'user_locality')
     click_button 'Update Profile'
     expect(page.current_path).to eq root_path
   end
 
   scenario 'valid user signup with a new location', js: true do
     visit new_user_path
-    fill_in 'Employee ID', with: '12345'
-    fill_in 'Address', with: 'No.322, Mars'
-    fill_in 'Phone No', with: '+91 9080706055'
-    find(:xpath, '//*[@id="new_user"]/div[4]/div/div/div[1]/input').set('Other')
-    find(:xpath, '//*[@id="new_user"]/div[4]/div/div/div[1]/input').native.send_keys(:return)
+    fill_in 'user_emp_id', with: '12345'
+    fill_in 'user_address', with: 'No.322, Mars'
+    fill_in 'user_phone_no', with: '+91 9080706055'
+    page.execute_script "window.scrollBy(0, 10000)"
+    find(:xpath, '//*[@id="new_user"]/div/div/div/input').set('Other')
+    find(:xpath, '//*[@id="new_user"]/div/div/div/input').native.send_keys(:return)
     fill_in 'otherBox', with: 'AF Station Yelahanka'
     page.execute_script "window.scrollBy(0, 10000)"
     click_button 'Update Profile'
     expect(page.current_path).to eq '/users'
     page.document.has_content? 'locality already exists'
-    find(:xpath, '//*[@id="new_user"]/div[4]/div/div/div[1]/input').set('Other')
-    find(:xpath, '//*[@id="new_user"]/div[4]/div/div/div[1]/input').native.send_keys(:return)
+    page.execute_script "window.scrollBy(0, 10000)"
+    find(:xpath, '//*[@id="new_user"]/div/div/div/div/input').set('Other')
+    find(:xpath, '//*[@id="new_user"]/div/div/div/div/input').native.send_keys(:return)
     fill_in 'otherBox', with: 'Jupiter'
     page.execute_script "window.scrollBy(0, 10000)"
     click_button 'Update Profile'
