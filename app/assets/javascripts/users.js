a8f08f86d15342697b9dde60e149d837a6873ad8
sandeeph <sandeeph@thoughtworks.com>
2016-01-19 14:27:53 +0530

    [Sandeep] Fixed js issue other locality

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,10 +1,11 @@
 var UserListener = {
     addListeners: function () {
-        $('#user_locality').selectize();
         var $select = $('div.full-width-selectize:first > select').selectize();
-        var selectize = $select[0].selectize;
+        if($select[0] != null)
+            var selectize = $select[0].selectize;
         var location = $('#user_locality_name');
-        selectize.setValue(selectize.search(location[0].value).items[0].id);
+        if(location[0] != null)
+            selectize.setValue(selectize.search(location[0].value).items[0].id);
 
         $('#user_locality').on('change', function () {
             if ($(this).val() === '-1') {
@@ -18,9 +19,11 @@ var UserListener = {
 };
 
 $(document).ready(function () {
+    $('#user_locality').selectize();
     UserListener.addListeners();
 });
 
 $(document).on('page:load', function () {
+    $('#user_locality').selectize();
     UserListener.addListeners();
 });
