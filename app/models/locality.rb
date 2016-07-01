fee8b39a574472a865c22a0c9216526e7a373cdd
Vijay Raghavan Aravamudhan <avijayr - at - gmail - dot - com>
2016-07-01 06:49:49 +0530

    Revert "Revert "Vijay: Localities are sorted by default when retrieving from the db.""
    This reverts commit 46ba37c9482314b96e41cba5ff9423085bfd6ff3.

--- app/models/locality.rb
+++ app/models/locality.rb
@@ -10,9 +10,10 @@
 #
 
 class Locality < ActiveRecord::Base
-  has_and_belongs_to_many :cabpools
-  validates :name, presence: true, uniqueness: true # TODO: Case-sensitivity is not validated?
   has_many :users
+  has_and_belongs_to_many :cabpools
 
-  # TODO: default order? this is being used as Locality.all.order in some places
+  validates :name, presence: true, uniqueness: true, case_sensitive: false
+
+  default_scope { order(:name) }
 end
