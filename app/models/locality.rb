6f536de48e5eb06d1e2130a671f27edb6b58c6ef
Vijay Raghavan Aravamudhan <avijayr - at - gmail - dot - com>
2016-07-03 11:45:34 +0530

    Vijay: Turn off Case-sensitivity for uniqueness verification for locality name.

--- app/models/locality.rb
+++ app/models/locality.rb
@@ -10,9 +10,10 @@
 #
 
 class Locality < ActiveRecord::Base
-  has_and_belongs_to_many :cabpools
-  validates :name, presence: true, uniqueness: true # TODO: Case-sensitivity is not validated?
   has_many :users
+  has_and_belongs_to_many :cabpools
+
+  validates :name, presence: true, uniqueness: true, case_sensitive: false
 
   # TODO: default order? this is being used as Locality.all.order in some places
 end
